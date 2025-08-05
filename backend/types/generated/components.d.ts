2f82955f69535fd94dd045f8cd0ab9dca2223b4a
Szymon Kubiak <kubiakszymonp@gmail.com>
2025-08-05 21:55:08 +0200

    init

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,102 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface AtomicBanner extends Struct.ComponentSchema {
+  collectionName: 'components_atomic_banners';
+  info: {
+    displayName: 'banner';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultHtml';
+        }
+      >;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    linkUrl: Schema.Attribute.String;
+  };
+}
+
+export interface AtomicButton extends Struct.ComponentSchema {
+  collectionName: 'components_atomic_buttons';
+  info: {
+    displayName: 'button';
+  };
+  attributes: {
+    link: Schema.Attribute.String;
+    text: Schema.Attribute.String;
+  };
+}
+
+export interface AtomicCard extends Struct.ComponentSchema {
+  collectionName: 'components_atomic_cards';
+  info: {
+    displayName: 'card';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    linkText: Schema.Attribute.String;
+    linkUrl: Schema.Attribute.String;
+    shortText: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface AtomicHeader extends Struct.ComponentSchema {
+  collectionName: 'components_atomic_headers';
+  info: {
+    displayName: 'header';
+  };
+  attributes: {
+    subtitle: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface NavigationItem extends Struct.ComponentSchema {
+  collectionName: 'components_navigation_items';
+  info: {
+    displayName: 'item';
+  };
+  attributes: {
+    link: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+    subItems: Schema.Attribute.Component<'navigation.sub-item', true>;
+  };
+}
+
+export interface NavigationSubItem extends Struct.ComponentSchema {
+  collectionName: 'components_navigation_sub_items';
+  info: {
+    displayName: 'subItem';
+  };
+  attributes: {
+    link: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+  };
+}
+
+export interface OrganismsArticleListing extends Struct.ComponentSchema {
+  collectionName: 'components_organisms_article_listings';
+  info: {
+    displayName: 'article listing';
+  };
+  attributes: {
+    articles: Schema.Attribute.Relation<'oneToMany', 'api::article.article'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'atomic.banner': AtomicBanner;
+      'atomic.button': AtomicButton;
+      'atomic.card': AtomicCard;
+      'atomic.header': AtomicHeader;
+      'navigation.item': NavigationItem;
+      'navigation.sub-item': NavigationSubItem;
+      'organisms.article-listing': OrganismsArticleListing;
+    }
+  }
+}
